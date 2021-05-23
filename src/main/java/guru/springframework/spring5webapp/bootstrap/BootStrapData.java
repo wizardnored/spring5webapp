3f8be9780b7a5ad856d7427cad7ddb2ae5d88897
Nored <antoinederon@gmail.com>
2021-05-23 17:14:35 +0200

    Exercice 2 : Add Publisher Entity Create new POJO for publisher
    Should have name and address (Address line 1, city, state, zip)
    
    Annotate publisher as Entity
    
    Setup Id Property
    
    Override equals, hashcode and toString
    
    Add Repository
    
    Create Publisher in Bootstrap class & verify output

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -5,20 +5,24 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
 	private final AuthorRepository authorRepository;
 	private final BookRepository bookRepository;
-	
-	
-	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+	private final PublisherRepository publisherRepository;
+
+
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		super();
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	@Override
@@ -27,20 +31,29 @@ public class BootStrapData implements CommandLineRunner {
 		Book ddd = new Book("Domain Driven Design", "123123");
 		eric.getBooks().add(ddd);	
 		ddd.getAuthors().add(eric);
-		
+
 		authorRepository.save(eric);
 		bookRepository.save(ddd);
-		
+
 		Author rod = new Author("Rod", "Johnson");
 		Book noEJB = new Book("J2EE Development without EJB", "987654987654");
 		rod.getBooks().add(noEJB);
 		noEJB.getAuthors().add(rod);
-		
+
 		authorRepository.save(rod);
 		bookRepository.save(noEJB);
-		
+
 		System.out.println("Started in Bootstrap");
 		System.out.println("Number of Books: " + bookRepository.count());
+
+		Publisher publisher = new Publisher();
+		publisher.setName("Hachette");
+		publisher.setCity("Paris");
+		publisher.setState("France");
+
+		publisherRepository.save(publisher);
+
+		System.out.println("Publisher Count: " + publisherRepository.count());
 	}
 
 }
